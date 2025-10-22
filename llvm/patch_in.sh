8c56a60109eebcb2be9fce2d4d56c7f14be7a1f6
yjj <yanglassnake@gmail.com>
2025-10-22 17:46:43 +0800

    add new RVV instruction vwcmul, vrotate

--- llvm/patch_in.sh
+++ llvm/patch_in.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git checkout llvmorg-17.0.6
 git am 0001-add-my-own-extension-xstar-with-a-single-instruction.patch
+git am 0002-add-vwcmul-and-vrotate-instruction-for-complex-numbe.patch
--- spike/patch_in.sh
+++ spike/patch_in.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git checkout 76858cc3aa178446584eae52f82e0a933e71f8db
 git am 0001-add-my-own-insn-xmac.patch
+git am 0002-add-support-to-vwcmul-and-vrotate-for-complex-number.patch
