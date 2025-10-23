419e301ed1794f69acdd340d92d97bf1b640267c
yjj <yanglassnake@gmail.com>
2025-10-23 16:57:53 +0800

    use CUSTOM OPCODE 3

--- llvm/patch_in.sh
+++ llvm/patch_in.sh
@@ -2,3 +2,4 @@
 git checkout llvmorg-17.0.6
 git am 0001-add-my-own-extension-xstar-with-a-single-instruction.patch
 git am 0002-add-vwcmul-and-vrotate-instruction-for-complex-numbe.patch
+git am 0003-use-CUSTOM-OPCODE3-for-this-xstar-extension.patch
